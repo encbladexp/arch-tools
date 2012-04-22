b03c079a269b657634507f370ac62a4666ec410c
Stefan J. Betz <info@stefan-betz.net>
2012-04-22 15:05:09 +0200

    Initial commit

---
+++ archstats.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ALL=`pacman -Q | wc -l`
+EXPL=`pacman -Qe | wc -l`
+LOCAL=`pacman -Qm | wc -l`
+NOREQ=`pacman -Qqdt | wc -l`
+KERNEL=`uname -r`
+GCC=`gcc --version | head -n1 | cut -c 11-38`
+SETUP=`grep -i 'installed filesystem' /var/log/pacman.log | cut -c 2-17`
+
+echo "$ALL total installed packages."
+echo "$EXPL explicitly installed packages."
+echo "$LOCAL localy installed packages."
+echo "$NOREQ packages installed as dependencies which are no longer needed."
+echo "Your system was originally set up $SETUP."
+echo "You are using kernel $KERNEL and gcc version $GCC."
