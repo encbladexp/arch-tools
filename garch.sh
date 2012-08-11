95f326633e7cdec8f8ba0e496bbe22714f6423f3
Stefan J. Betz <info@stefan-betz.net>
2012-08-11 10:19:13 +0200

    Added simple pacstrap based installation tool.

---
+++ garch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Setup Script for a mostly German Arch Linux Installation.
+echo "Bootstrapping Base System..."
+pacstrap /mnt base base-devel vim bash-completion htop grub-bios sudo
+echo "...done"
+echo "Creating default configuration..."
+genfstab -L /mnt >> /mnt/etc/fstab
+cat > /mnt/etc/hostname << EOF
+YOURHOSTNAME
+EOF
+cat > /mnt/etc/vconsole.conf << EOF
+KEYMAP=de-latin1-nodeadkeys
+EOF
+cat > /mnt/etc/locale.conf << EOF
+LANG=en_US.UTF-8
+LC_MESSAGES=C
+EOF
+echo "...done"
+echo "Editor session..."
+vim /mnt/etc/rc.conf
+vim /mnt/etc/mkinitcpio.conf
+vim /mnt/etc/default/grub
+vim /mnt/etc/locale.gen
+vim /mnt/etc/hostname
+echo "...done"
+echo "Chroot session..."
+cat > /mnt/garch-setup << EOF
+#!/bin/bash
+pacman -R vi nano
+ln -s /usr/bin/vim /usr/bin/nano
+ln -s /usr/bin/vim /usr/bin/vi
+ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+locale-gen
+mkinitcpio -p linux
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install /dev/sda
+passwd
+EOF
+chmod 0755 /mnt/garch-setup
+arch-chroot /mnt /garch-setup
+echo "...done"
+echo "Time for reboot: NOW ;-)"
