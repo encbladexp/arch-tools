de151e9cb37993a596fdf0864d6e7489af4906ca
Stefan J. Betz <info@stefan-betz.net>
2014-01-03 18:28:06 +0100

    Commited current garch draft (for github migration)

--- garch.sh
+++ garch.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-# Setup Script for a mostly German Arch Linux Installation.
-#echo "Mounting Network Cache..."
-#mkdir -p /mnt/var/cache/pacman/pkg
-#mkdir -p /var/cache/pacman/pkg
-#mount server:/pacman /mnt/var/cache/pacman/pkg
-#mount server:/pacman /var/cache/pacman/pkg
-#echo "...done"
+# Quick Setup Script for a mostly German Arch Linux Installation.
+# Dangerous at this moment, only run this when you know what you do!!!
+exit 1
+echo "Disable exisiting LVM Volumes..."
+vgchange -an
+echo "...done"
 echo "Installation Editor..."
-pacman -Sy
+#pacman -Sy
 pacman --noconfirm -S vim
 echo "...done"
+echo "Creating Partition Table, LVM and Filesystems..."
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sda mkpart primary 1M 4M
+parted -s /dev/sda set 1 bios_grub on
+parted -s /dev/sda mkpart primary 4M 100M
+parted -s /dev/sda set 2 boot on
+parted -s /dev/sda mkpart primary 100M 100%
+parted -s /dev/sda set 3 lvm on
+partprobe
+pvcreate /dev/sda3
+vgcreate main /dev/sda3
+lvcreate -L 5G -n root main
+mke2fs -t ext4 -L ROOT /dev/main/root
+mount /dev/main/root /mnt
+mkdir /mnt/boot
+mount /dev/sda2 /mnt/boot
+echo "...done"
 echo "Bootstrapping Base System..."
-pacstrap /mnt base base-devel vim bash-completion htop grub-bios sudo nfs-utils
+pacstrap -c /mnt base base-devel vim bash-completion htop grub-bios sudo openssh nfs-utils
 echo "...done"
 echo "Creating default configuration..."
 genfstab -L /mnt >> /mnt/etc/fstab
@@ -25,6 +41,11 @@ cat > /mnt/etc/locale.conf << EOF
 LANG=en_US.UTF-8
 LC_MESSAGES=C
 EOF
+echo "...done"
+echo "Patching config files..."
+#- patch mkinitcpio.conf
+#- patch locale.gen
+#- patch etc/default/grub
 echo "...done"
 echo "Editor session..."
 vim /mnt/etc/mkinitcpio.conf
@@ -41,11 +62,13 @@ ln -s /usr/bin/vim /usr/bin/vi
 ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 locale-gen
 mkinitcpio -p linux
+passwd
 grub-mkconfig -o /boot/grub/grub.cfg
 grub-install /dev/sda
-passwd
 EOF
 chmod 0755 /mnt/garch-setup
 arch-chroot /mnt /garch-setup
+rm /mnt/garch-setup
+done
 echo "...done"
 echo "Time for reboot: NOW ;-)"
